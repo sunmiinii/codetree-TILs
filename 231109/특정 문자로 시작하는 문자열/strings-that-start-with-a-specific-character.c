f5f74d8610633bb15dbc5889f510b20cc3cde3b6
sunmiinii <139675194+sunmiinii@users.noreply.github.com>
2023-11-09 21:05:48 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.c
+++ 231109/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.c
@@ -2,29 +2,32 @@
 #include <string.h>
 
 int main() {
+    // 문자열 배열을 구현합니다.
 	char str[20][21];
 	char a;
-	int i,lens= 0;
-	int len, cnt = 0;
+	int len_sum = 0;
+	int cnt = 0;
 
+	// 입력
 	int n;
 	scanf("%d", &n);
 
-	for(i = 0; i < n; i++)
+	for(int i = 0; i < n; i++)
 		scanf("%s", str[i]);
 
 	scanf(" %c", &a);	
 	
-
-	for(i = 0; i < n; i++) {
+	// 조건에 맞는 문자열의 총 길이와 개수를 계산해줍니다.
+	for(int i = 0; i < n; i++) {
 		if(str[i][0] == a) {
-			len = strlen(str[i]);
-			lens= len;
+			int len = strlen(str[i]);
+			len_sum += len;
 			cnt++;
 		}
     }
-
-	printf("%d %.2f", cnt, (double)lens / cnt);
+  
+	// 구하고자 하는 값을 출력합니다.
+	printf("%d %.2f", cnt, (double)len_sum / cnt);
 	
     return 0;
 }
