a19656200ef154e27604462c69c17f11f2643ecf
sunmiinii <139675194+sunmiinii@users.noreply.github.com>
2023-11-09 21:04:40 +0900

    Title: 특정 문자로 시작하는 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231109/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char str[20][21];
+	char a;
+	int i,lens= 0;
+	int len, cnt = 0;
+
+	int n;
+	scanf("%d", &n);
+
+	for(i = 0; i < n; i++)
+		scanf("%s", str[i]);
+
+	scanf(" %c", &a);	
+	
+
+	for(i = 0; i < n; i++) {
+		if(str[i][0] == a) {
+			len = strlen(str[i]);
+			lens= len;
+			cnt++;
+		}
+    }
+
+	printf("%d %.2f", cnt, (double)len_sum / cnt);
+	
+    return 0;
+}
