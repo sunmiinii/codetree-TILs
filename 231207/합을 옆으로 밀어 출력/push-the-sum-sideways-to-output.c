fe4e1052389b4bb3a3ec744353bbca994c9db49e
sunmiinii <139675194+sunmiinii@users.noreply.github.com>
2023-12-07 14:34:17 +0900

    Title: 합을 옆으로 밀어 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231207/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.c
+++ 231207/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 int main() {
     int n,i,a;
+    int *p;
     int sum=0;
+    char result[12];
     scanf("%d",&n);
+    p=(int *)malloc(sizeof(int)*n);
     for(i=0;i<n;i++){
-        scanf("%d",&a);
-
-        sum=sum+a;
+        scanf("%d",&p[i]);
+        sum+=p[i];
     }
-    char sum_str[100];
-    
-    sprintf(sum_str,"%d",sum);
-    printf(sum+str[1]);
-    printf("%s",sum_str);
+    sprintf(result,"%d",sum);
+    printf("%s%c",result+1,result[0]);
+    free(p);
     // 여기에 코드를 작성해주세요.
     return 0;
 }
