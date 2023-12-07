33b3bd8eca23be9291105ec1f735f574ad0d9ad7
sunmiinii <139675194+sunmiinii@users.noreply.github.com>
2023-12-07 13:24:44 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
+++ 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-
-
-//p1=(int *)malloc(sizeof(int)*n)
-
-int main() {
+#include <stdlib.h>
+int main(){
     int n;
+    int *p;
     int i;
     int cnt=0;
-    int arr[100];
     scanf("%d",&n);
+    p=(int *)malloc(sizeof(int)*n);
     for(i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-        if(arr[i]==2)
+        scanf("%d",&p[i]);
+        if(p[i]==2){
             cnt++;
-        if(cnt==3){
-            printf("%d",i+1);
-            break;
-        
+            if(cnt==3){
+                printf("%d",i+1);
+                break;
+            }
         }
     }
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
